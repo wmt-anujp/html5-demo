75f52a556fc403e44d3dac4683f8a037341d158d
Anuj Panchal <anujp@webmob.tech>
2022-01-20 17:53:49 +0530

    new js files

---
+++ try.js
@@ -0,0 +1,41 @@
+var city = "Delhi";
+console.log(city)
+
+const ownerName = "Anuj Panchal";
+console.log(ownerName)
+
+{
+    // let city = "Ahmedabad";
+    // city = "Surat";
+    console.log(city);
+}
+
+
+let nullVar = null;
+console.log("Datatype is: " + (typeof(nullVar)));
+
+let myArr = [1, 2, 3, 4, false, "string"];
+console.log("Datatype is: " + (typeof(myArr)) + " " + myArr);
+
+
+
+let studentMarks = {
+    "anuj panchal": 85,
+    chirag: 78,
+    janaki: 67
+}
+console.log(studentMarks);
+console.log("dataype is: " + (typeof studentMarks));
+
+
+function findName() {
+
+}
+console.log("Dataype is:" + typeof findName);
+
+let date = new Date();
+console.log(date);
+console.log("Dataype is:" + typeof date);
+
+let firstName;
+console.log(firstName);
