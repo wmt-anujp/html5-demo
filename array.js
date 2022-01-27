1c4f33cefc6cf2d6940c135e5bca20a311164989
Anuj Panchal <anujp@webmob.tech>
2022-01-27 18:50:49 +0530

    changes

--- array.js
+++ array.js
@@ -1,27 +1,28 @@
-// let marks = [34, 35, 85, 75, 41, 52];
+let marks = [34, 35, 85, 75, 41, 52];
 const fruits = ["Orange", "Apple", "Stawberry"];
 const mixed = ["str", 856, [56, 78]];
 
 const arr = new Array(56, 56, 78, 98, "Orange");
-// console.log(arr);
-// console.log(marks);
-// console.log(fruits[2]);
-// console.log(mixed);
-// console.log(arr.length);
-// console.log(Array.isArray(marks));
+console.log(arr);
+console.log(marks);
+console.log(fruits[2]);
+console.log(mixed);
+console.log(arr.length);
+console.log(Array.isArray(marks));
 
-// let value = marks.indexOf(85);
-// console.log(value);
+let value = marks.indexOf(85);
+console.log(value);
 
-// marks.push(100);
-// marks.unshift(99)
-// marks.pop()
-// marks.shift()
-// marks.splice(0, 3)
-// marks.reverse()
+marks.push(100);
+marks.unshift(99)
+marks.pop()
+marks.shift()
+marks.splice(0, 3)
+marks.reverse()
+delete marks[2];
 let marks2 = [1, 4, 5, 8, 6]
-// marks=marks.concat(marks2)
-// console.log(marks)
+marks=marks.concat(marks2)
+console.log(marks)
 
 let myobj = {
     "name": "Anuj",
@@ -31,3 +32,8 @@ let myobj = {
 
 console.log(myobj.surname)
 console.log(myobj['marks'])
+
+const numbers = [34, 23, 12, 56, 78];
+numbers.forEach((item, index, arr) => {
+    console.log('a[' + index + ']=' + item);
+})
